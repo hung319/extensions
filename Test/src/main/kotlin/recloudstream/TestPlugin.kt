b4ee256ee20fbb4cfc2d62c3eee7fc627cd201a9
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-05-24 07:20:49 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -1,4 +1,4 @@
-package com.example.nangcucprovider
+package com.heovl
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
@@ -11,6 +11,6 @@ class TestPlugin: Plugin() { // Kế thừa Plugin
     override fun load(context: Context) {
         // Tất cả provider nên được thêm vào theo cách này.
         // Đăng ký AnimeHayProvider
-        registerMainAPI(NangCucProvider()) // Gọi đăng ký provider ở đây
+        registerMainAPI(HeoVLProvider()) // Gọi đăng ký provider ở đây
     }
 }
