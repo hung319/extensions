7b9e5c97a010adea102123b5b09cda19d3ef93d4
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-06-13 21:49:36 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -3,15 +3,14 @@ package com.lagradost.cloudstream3.hentai.providers
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-// import com.lagradost.cloudstream3.plugins.PluginManager.* // Import này có thể cần thiết
 
 @CloudstreamPlugin
 class TestPlugin: Plugin() {
     override fun load(context: Context) {
         // Đăng ký Provider để duyệt và tìm kiếm
-        registerMainAPI(TestProvider()) // hoặc IHentaiProvider() nếu bạn dùng tên đó
+        registerMainAPI(IHentaiProvider()) // Đảm bảo tên lớp này khớp với file trên
 
-        // *** Đăng ký Extractor để xử lý link ảo .local ***
+        // Đăng ký Extractor để xử lý link ảo .local
         registerExtractorAPI(IHentaiExtractor())
     }
 }
