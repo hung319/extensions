1b07f126960cb23da99e3f95c78b7f0cc031f33a
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-06-13 21:49:10 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -1,16 +1,17 @@
-package recloudstream 
+package com.lagradost.cloudstream3.hentai.providers
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-// Import này có thể cần thiết nếu registerMainAPI là extension function
-// import com.lagradost.cloudstream3.plugins.PluginManager.registerMainAPI
+// import com.lagradost.cloudstream3.plugins.PluginManager.* // Import này có thể cần thiết
 
-@CloudstreamPlugin // Đánh dấu đây là plugin
-class TestPlugin: Plugin() { // Kế thừa Plugin
+@CloudstreamPlugin
+class TestPlugin: Plugin() {
     override fun load(context: Context) {
-        // Tất cả provider nên được thêm vào theo cách này.
-        // Đăng ký AnimeHayProvider
-        registerMainAPI(IhentaiProvider()) // Gọi đăng ký provider ở đây
+        // Đăng ký Provider để duyệt và tìm kiếm
+        registerMainAPI(TestProvider()) // hoặc IHentaiProvider() nếu bạn dùng tên đó
+
+        // *** Đăng ký Extractor để xử lý link ảo .local ***
+        registerExtractorAPI(IHentaiExtractor())
     }
 }
