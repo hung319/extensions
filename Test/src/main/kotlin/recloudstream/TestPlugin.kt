648207e02027f2407ce8ccb0ce961a1729f12c46
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-06-20 17:42:00 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -11,6 +11,6 @@ class TestPlugin: Plugin() { // Kế thừa Plugin
     override fun load(context: Context) {
         // Tất cả provider nên được thêm vào theo cách này.
         // Đăng ký AnimeHayProvider
-        registerMainAPI(Rule34VideoProvider()) // Gọi đăng ký provider ở đây
+        registerMainAPI(AnimeVietsubProvider()) // Gọi đăng ký provider ở đây
     }
 }
