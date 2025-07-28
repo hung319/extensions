9cb5def4fb7145a588a9be26fe366248d9776916
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-07-28 17:29:39 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -1,4 +1,4 @@
-package com.recloudstream.extractors 
+package recloudstream 
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
@@ -11,6 +11,6 @@ class TestPlugin: Plugin() { // Kế thừa Plugin
     override fun load(context: Context) {
         // Tất cả provider nên được thêm vào theo cách này.
         // Đăng ký AnimeHayProvider
-        registerMainAPI(Fpo()) // Gọi đăng ký provider ở đây
+        registerMainAPI(BluPhimProvider()) // Gọi đăng ký provider ở đây
     }
 }
