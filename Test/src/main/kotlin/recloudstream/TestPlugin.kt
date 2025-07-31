384a082d89d5798fd3e0223e584f0256f7b07688
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-08-01 06:54:39 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -11,6 +11,6 @@ class TestPlugin: Plugin() { // Kế thừa Plugin
     override fun load(context: Context) {
         // Tất cả provider nên được thêm vào theo cách này.
         // Đăng ký AnimeHayProvider
-        registerMainAPI(PerfectGirlsProvider()) // Gọi đăng ký provider ở đây
+        registerMainAPI(SextbProvider()) // Gọi đăng ký provider ở đây
     }
 }
