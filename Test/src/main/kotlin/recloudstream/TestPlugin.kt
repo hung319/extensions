b6d6c088cfa6ea18cfb352dd6c67fa10b88f8611
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-08-19 18:45:11 +0700

    Update TestPlugin.kt

--- Test/src/main/kotlin/recloudstream/TestPlugin.kt
+++ Test/src/main/kotlin/recloudstream/TestPlugin.kt
@@ -11,6 +11,6 @@ class TestPlugin: Plugin() { // Kế thừa Plugin
     override fun load(context: Context) {
         // Tất cả provider nên được thêm vào theo cách này.
         // Đăng ký AnimeHayProvider
-        registerMainAPI(SDFimProvider()) // Gọi đăng ký provider ở đây
+        registerMainAPI(KuraKura21Provider()) // Gọi đăng ký provider ở đây
     }
 }
